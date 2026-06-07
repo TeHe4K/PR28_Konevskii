726ce01e1b51ce76d9925f9be8e431463a6d1b56
TeHe4K <hryblackwii@gmail.com>
2026-06-07 16:08:40 +0500

    Разработан класс подключения к бд. Коневский

---
+++ PR28_Konevskii/Classes/Common/Connection.cs
@@ -0,0 +1,24 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace PR28_Konevskii.Classes.Common
+{
+    public class Connection
+    {
+        public static readonly string config = "server=127.0.0.1;uid=root;pwd=;database=pc_clubs";
+        public static MySqlConnection OpenConnection()
+        {
+            MySqlConnection connection = new MySqlConnection(config);
+            connection.Open();
+            return connection;
+        }
+        public static MySqlDataReader Query(string SQL, MySqlConnection connection)
+        {
+            return new MySqlCommand(SQL, connection).ExecuteReader();
+        }
+        public static void CloseConection(MySqlConnection connection)
+        {
+            connection.Close();
+            MySqlConnection.ClearAllPools();
+        }
+    }
+}
