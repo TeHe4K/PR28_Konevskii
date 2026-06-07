6d94ee7219c4ba09954fdf4c167a6d2c02e13a22
TeHe4K <hryblackwii@gmail.com>
2026-06-07 16:04:40 +0500

    Разработана модель для пк клубов. Коневский

---
+++ PR28_Konevskii/Models/pcclub.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace PR28_Konevskii.Models
+{
+    public class pcclub
+    {
+        public int id {  get; set; }
+        public string name { get; set; }
+        public string adress { get; set; }
+        public DateTime time_start { get; set; }
+        public DateTime time_end { get; set; }
+
+        public pcclub(int id, string name, string adress, DateTime time_start, DateTime time_end)
+        {
+            this.id = id;
+            this.name = name;
+            this.adress = adress;
+            this.time_start = time_start;
+            this.time_end = time_end;
+        }
+    }
+}
