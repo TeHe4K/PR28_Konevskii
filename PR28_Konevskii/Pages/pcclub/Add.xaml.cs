621080f9c0cc821e18d9faf4ccd2f9606334a13c
TeHe4K <hryblackwii@gmail.com>
2026-06-07 20:18:42 +0500

    Разработана страница Add для PcClub. Коневский

--- PR28_Konevskii/Pages/pcclub/Add.xaml.cs
+++ PR28_Konevskii/Pages/pcclub/Add.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PR28_Konevskii.Classes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,71 @@ namespace PR28_Konevskii.Pages.pcclub
     /// </summary>
     public partial class Add : Page
     {
-        public Add(Classes.pcclublContext pcClub)
+        pcclublContext PcClub;
+        public Add(pcclublContext pcClub = null)
         {
             InitializeComponent();
+
+            if(PcClub != null)
+            {
+                this.PcClub = pcClub;
+                name.Text = pcClub.name;
+                adres.Text = pcClub.adress;
+                timeStart.Text = pcClub.time_start.ToString("HH:mm");
+                timeEnd.Text = pcClub.time_end.ToString("HH:mm");
+                bthAdd.Content = "Изменить";
+            }
+        }
+
+        private void AddRecord(object sender, RoutedEventArgs e)
+        {
+            DateTime TimeStart;
+            DateTime TimeEnd;
+            if (name.Text == "")
+            {
+                MessageBox.Show("Необходимо указать наименование");
+                return;
+            }
+            if(adres.Text == "")
+            {
+                MessageBox.Show("Необходимо указать адрес");
+                return;
+            }
+            if(timeStart.Text == "" || DateTime.TryParse(timeStart.Text, out TimeStart)){
+                MessageBox.Show("Необходимо указать время начала работы");
+                return;
+            }
+            if (timeEnd.Text == "" || DateTime.TryParse(timeEnd.Text, out TimeEnd )){
+                MessageBox.Show("Необходимо указать время окончания работы");
+                return;
+            }
+
+            if(PcClub == null)
+            {
+                pcclublContext newPcClub = new pcclublContext(
+                    0,
+                    name.Text,
+                    adres.Text,
+                    TimeStart,
+                    TimeEnd
+                    );
+                newPcClub.Add();
+                MessageBox.Show("Запись успешно добавлена");
+                MainWindow.init.OpenPage(new Pages.pcclub.Main());
+            }
+            else
+            {
+                PcClub = new pcclublContext(
+                    PcClub.id,
+                    name.Text,
+                    adres.Text,
+                    TimeStart,
+                    TimeEnd
+                    );
+                PcClub.Update();
+                MessageBox.Show("Запись успешно обновлена");
+                MainWindow.init.OpenPage(new Pages.pcclub.Main());
+            }
         }
     }
 }
