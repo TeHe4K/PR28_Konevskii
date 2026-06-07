096e7a88af97e2a16a9d60f49b8c0c81b1438e05
TeHe4K <hryblackwii@gmail.com>
2026-06-07 16:06:12 +0500

    Разработана модель аренды компьютеров

---
+++ PR28_Konevskii/Models/pcrent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace PR28_Konevskii.Models
+{
+    public class pcrent
+    {
+        public int Id { get; set; }
+        public string FioRent { get; set; }
+        public DateTime DateRent { get; set; }
+        public DateTime TimeRent { get; set; }
+
+        public pcrent(int id, string fioRent, DateTime dateRent, DateTime timeRent)
+        {
+            Id = id;
+            FioRent = fioRent;
+            DateRent = dateRent;
+            TimeRent = timeRent;
+        }
+    }
+}
